d71907ac3b4ad2287c24a973f9bc9f8909bf60f5
extf8 <extfield8@gmail.com>
2024-04-23 16:37:32 +0300

    initial commit

---
+++ HashNode.js
@@ -0,0 +1,15 @@
+/**
+ * Represents a node in a hash map.
+ */
+export class HashNode {
+    /**
+     * Creates a new instance of HashNode.
+     * @param {*} key - The key associated with the value.
+     * @param {*} value - The value to be stored in the node.
+     */
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.nextNode = null;
+    }
+}
