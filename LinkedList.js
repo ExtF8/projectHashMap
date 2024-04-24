3e35f7faa8565a0c8c5bd663dd1e166eaa160d48
extf8 <extfield8@gmail.com>
2024-04-24 12:09:25 +0300

    feat: refactor LinkedList class for use in HashMap and HashSet

--- LinkedList.js
+++ LinkedList.js
@@ -14,8 +14,8 @@ export class LinkedList {
     }
 
     // Adds a new node containing value to the end of the list
-    append(value) {
-        const newNode = new HashNode(value);
+    append(key, value) {
+        const newNode = new HashNode(key, value);
         if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
@@ -23,17 +23,6 @@ export class LinkedList {
         }
         this.tail.nextNode = newNode;
         this.tail = newNode;
-    }
-
-    // Adds a new node containing value to the start of the list
-    prepend(value) {
-        const newNode = new HashNode(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        }
-        newNode.nextNode = this.head;
-        this.head = newNode;
     }
 
     // Returns the total number of nodes in the list
@@ -50,12 +39,12 @@ export class LinkedList {
     }
 
     // Returns the first node of the list
-    head() {
+    getHead() {
         return this.head;
     }
 
     // Returns the last node of the list
-    tail() {
+    getTail() {
         return this.tail;
     }
 
@@ -84,59 +73,6 @@ export class LinkedList {
         }
     }
 
-    // Removes the last element from the list
-    pop() {
-        // List empty
-        if (!this.head) {
-            return;
-        }
-
-        // Is last element
-        if (!this.head.nextNode) {
-            this.head = null;
-            this.tail = null;
-            return;
-        }
-
-        let current = this.head;
-        while (current.nextNode !== this.tail) {
-            current = current.nextNode;
-        }
-
-        current.nextNode = null;
-        this.tail = current;
-    }
-
-    // Returns true if the passed value is in the list otherwise returns false
-    contains(value) {
-        let current = this.head;
-
-        while (current) {
-            if (current.value == value) {
-                return true;
-            }
-            current = current.nextNode;
-        }
-
-        return false;
-    }
-
-    // Returns the index of the node containing value, or null if not found
-    find(value) {
-        let current = this.head;
-        let index = 0;
-
-        while (current) {
-            if (current.value == value) {
-                return index;
-            }
-            current = current.nextNode;
-            index++;
-        }
-
-        return null;
-    }
-
     // Represents LinkedList objects as strings, to print them
     // The format: ( value ) -> ( value ) -> ( value ) -> null
     toString() {
@@ -150,79 +86,6 @@ export class LinkedList {
 
         result += 'null';
         return result;
-    }
-
-    // Inserts a new node with provided value at the given index
-    insertAt(value, index) {
-        if (index < 0) {
-            return;
-        }
-
-        const newNode = new HashNode(value);
-
-        // At the beginning of the list
-        if (index === 0) {
-            newNode.nextNode = this.head;
-            this.head = newNode;
-
-            if (!this.tail) {
-                this.tail = newNode;
-            }
-
-            return;
-        }
-
-        let current = this.head;
-        let previous = null;
-        let currentIndex = 0;
-
-        while (current && currentIndex < index) {
-            previous = current;
-            current = current.nextNode;
-            currentIndex++;
-        }
-
-        if (currentIndex === index) {
-            previous.nextNode = newNode;
-            newNode.nextNode = current;
-            if (!current) {
-                this.tail = newNode;
-            }
-        }
-    }
-
-    // Removes the node at the given index
-    removeAt(index) {
-        if (index < 0 || !this.head) {
-            return;
-        }
-
-        if (index === 0) {
-            this.head = this.head.nextNode;
-            // If the list becomes empty
-            if (!this.head) {
-                this.tail = null;
-            }
-            return;
-        }
-
-        let current = this.head;
-        let previous = null;
-        let currentIndex = 0;
-
-        while (current && currentIndex < index) {
-            previous = current;
-            current = current.nextNode;
-            currentIndex++;
-        }
-
-        if (currentIndex === index && current) {
-            previous.nextNode = current.nextNode;
-            // If removed node was tail
-            if (!current.nextNode) {
-                this.tail = previous;
-            }
-        }
     }
 
     // List all nodes
